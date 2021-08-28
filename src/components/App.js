28393e3dce7899c9357c920c07f1f4ef2592a4d0
Yash Pratap Singh <singhyashpratap06@gmail.com>
2021-08-28 16:37:11 +0530

    Add post mutation to create links

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import CreateLink from './CreateLinks'
 import LinkList from './LinkList'
 
-const App = () => <LinkList />
+const App = () => <CreateLink />
 
 export default App
