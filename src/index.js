4d4be5a8a916f0d1023997e5084dafb4f5c24a6d
Yash Pratap Singh <singhyashpratap06@gmail.com>
2021-08-28 20:29:57 +0530

    add authentication usign fetchOptions for sending headers

--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@ import './styles/index.css'
 import App from './components/App'
 
 import { Provider, Client, defaultExchanges } from 'urql'
+import { getToken } from './token'
 
 const client = new Client({
   url: 'http://localhost:4000',
+  fetchOptions: () => {
+    const token = getToken();
+    return {
+      headers: { authorization: token ? `Bearer ${token}` : '' }
+    }
+  },
   exchanges: defaultExchanges
 })
+
 ReactDOM.render(
   <BrowserRouter>
     <Provider value={client}>
